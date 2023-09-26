92c5d6f15861932ff0110a845734da5adcac1a2a
Andres Henriquez <rymbell@gmail.com>
2023-09-26 12:12:30 +0900

    practica

---
+++ test.py
@@ -0,0 +1,34 @@
+line1 = '******************************'
+line2 = '*                            *'
+line3 = '            WELCOME           '
+line4 = 234
+
+print('')
+print(line1)
+print(line2)
+print(line3)
+print(line2)
+print(line1)
+
+#this is a comment
+# decade = str(1980)
+# print(type(decade))
+
+# long_string = '''
+# this is a very long string
+# in python
+# for writting
+# veryyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy
+# looooooooooooooooooooooooooooong
+# striiiiiiiiiiiiiiiiiiiiiiiiiiiiings
+# whit formatting and everything'''
+
+# print(long_string)
+
+title = 'menu'.upper()
+print('')
+print(title.center(30, '='))
+print("coffee".ljust(25, '.') + '$1'.rjust(4))
+print("tea".ljust(25, '.') + '$1'.rjust(4))
+print("Muffin".ljust(25, '.') + '$2'.rjust(4))
+print("Cheesecake".ljust(25, '.') + '$3'.rjust(4))
